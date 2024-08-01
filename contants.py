af437a18937b597794b26bd79e412693073c34cb
PWyman <paul.wyman@outlook.com>
2024-08-01 08:48:20 +0100

    Create contants.py

---
+++ contants.py
@@ -0,0 +1,6 @@
+# CONFIGURATION ################################################################
+
+
+INTERNET_NAME = "xxxxxxx"
+INTERNET_PASSWORD = "xxxxxxxxx"
+CHAT_GPT_API_KEY = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
